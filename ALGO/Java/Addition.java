299a2901a4e9a016ce88c9d1bea4fe1b00b52ef7
Léo Gillet <leo@leogillet.com>
2022-01-24 12:17:51 +0100

    fin du cours

--- ALGO/Java/Addition.java
+++
@@ -1,8 +0,0 @@
-import java.io.*;
-import java.util.*;
-public class Addition {
-	public static void main(String[] args) {
-		System.out.print("123+321 font ");
-		System.out.println(123+321);
-	}
-}
